2b202d948d25d19b1b238fd6282bfa120d5e77c0
OlVo2580 <124206194+OlVo2580@users.noreply.github.com>
2023-10-23 01:23:27 +0300

    first commit

---
+++ DZ_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using DZ_6;
+
+try
+{
+    const string InputPath = "C:\\Users\\volos\\OneDrive\\Рабочий стол\\К-27\\ООП\\DZ_6\\DZ_6\\input.json";
+    const string OutputPath = "C:\\Users\\volos\\OneDrive\\Рабочий стол\\К-27\\ООП\\DZ_6\\DZ_6\\output.json";
+
+    FileStream fs = new FileStream(InputPath, FileMode.OpenOrCreate);
+
+    var books = await JsonSerializer.DeserializeAsync<List<Book>>(fs);
+
+
+    foreach (var book in books)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(book));
+    }
+
+    fs.Close();
+
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+    };
+
+    File.WriteAllText(OutputPath, JsonSerializer.Serialize(books, options));
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
